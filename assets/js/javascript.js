d0730ab5506795ef7a2b2f324a26e495b507c358
Lorena <lorenaaros@gmail.com>
2018-11-30 23:39:18 -0300

    second commit 2do entregable

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,11 +1,35 @@
-$(function(){
-  alert("antes de la carga");
+//
+$(function() {
+  alert("previo de la carga");
 
+  //
+  $(".featured").attr("src", "assets/img/4.jpg")
 
-$(".featured").attr("src", "assets/img/4.jpg")
-
-$(".thumb").on("click", function(){
-  source = $(this).attr("src");
-  $(".featured").attr("src", source);
-})
+  $(".thumb").on("click", function() {
+    source = $(this).attr("src");
+    $(".featured").attr("src", source);
+  })
 });
+
+//
+document.getElementById("p3").addEventListener("click", function() {
+  alert("escríbeme:lorenaaros@gmail.com");
+});
+
+//
+
+$(document).ready(function() {
+  $("#boton").click(function() {
+    $("ul#listas li").prepend(function(n) {
+      return "--> Lista índice: " + n + ".";
+    });
+  });
+});
+
+//
+function myFunction() {
+  var node = document.createElement("LI");
+  var textnode = document.createTextNode("Ejercicios");
+  node.appendChild(textnode);
+  document.getElementById("myList").appendChild(node);
+}
